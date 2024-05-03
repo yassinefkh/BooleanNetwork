2f04d071fc3bdc33b4d07f5e9d04c7d7e7cf0030
Yassine <yassinefekih78@outlook.fr>
2024-05-03 23:18:35 +0200

    16 booleans functions

--- src/sommet.py
+++ src/sommet.py
@@ -20,16 +20,41 @@ class Sommet:
         self.historique_fonctions.append(self.func_index)
 
     def calculer_etat(self, inputs):
-        if self.func_index == 0:  # Fonction NAND
-            return not all(inputs)
-        elif self.func_index == 1:  # Fonction AND
+        if self.func_index == 0:  # NULL function (always False)
+            return False
+        elif self.func_index == 1:  # AND
             return all(inputs)
-        elif self.func_index == 2:  # Fonction XOR
+        elif self.func_index == 2:  # OR
+            return any(inputs)
+        elif self.func_index == 3:  # XOR
             return sum(inputs) % 2 == 1
-        elif self.func_index == 3:  # Fonction Identité
-            return inputs[0]  
+        elif self.func_index == 4:  # NAND
+            return not all(inputs)
+        elif self.func_index == 5:  # NOR
+            return not any(inputs)
+        elif self.func_index == 6:  # XNOR
+            return sum(inputs) % 2 == 0
+        elif self.func_index == 7:  # Identity of first input
+            return inputs[0]
+        elif self.func_index == 8:  # Inverse of first input
+            return not inputs[0]
+        elif self.func_index == 9:  # Identity of second input (or false if not available)
+            return inputs[1] if len(inputs) > 1 else False
+        elif self.func_index == 10:  # Inverse of second input (or false if not available)
+            return not inputs[1] if len(inputs) > 1 else False
+        elif self.func_index == 11:  # A AND NOT B
+            return inputs[0] and not inputs[1] if len(inputs) > 1 else False
+        elif self.func_index == 12:  # NOT A AND B
+            return not inputs[0] and inputs[1] if len(inputs) > 1 else False
+        elif self.func_index == 13:  # NOT A
+            return not inputs[0]
+        elif self.func_index == 14:  # NOT B
+            return not inputs[1] if len(inputs) > 1 else False
+        elif self.func_index == 15:  # TRUE (always True)
+            return True
         else:
             raise ValueError("Index de fonction non valide")
+
 
     def __str__(self):
         return str(int(self.etat))
